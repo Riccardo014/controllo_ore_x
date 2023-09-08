10822b58deb7b89e384a2bb3133b1c859c078e26
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-08 09:44:53 +0200

    Feat: added projects release table line

--- apps/controllo-ore-x/src/app/app.module.ts
+++ apps/controllo-ore-x/src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TrackerTableLineComponent } from './modules/auth/modules/tracker/components/tracker-table-line/tracker-table-line.component';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
+import { ProjectsReleaseTableLineComponent } from './modules/auth/modules/projects/components/projects-release-table-line/projects-release-table-line.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { TeamTableLineComponent } from './modules/auth/modules/team/components/team-table-line/team-table-line.component';
 
 @NgModule({
-  declarations: [AppComponent, TrackerTableLineComponent],
+  declarations: [
+    AppComponent,
+    TrackerTableLineComponent,
+    ProjectsReleaseTableLineComponent,
+    TeamTableLineComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
     MatChipsModule,
+    MatProgressBarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
