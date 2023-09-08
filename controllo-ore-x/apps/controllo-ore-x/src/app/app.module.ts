e964536d5e0baf3f001ceccf31ec78a3bcbab911
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-08 09:31:48 +0200

    Feat: added tracker table line

--- controllo-ore-x/apps/controllo-ore-x/src/app/app.module.ts
+++ controllo-ore-x/apps/controllo-ore-x/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrackerTableLineComponent } from './modules/auth/modules/tracker/components/tracker-table-line/tracker-table-line.component';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TrackerTableLineComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
