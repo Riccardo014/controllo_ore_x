596068e6bde2af37e59d09ac0ca7eda2f39cbffe
unknown <riky42001@gmail.com>
2023-09-11 11:36:01 +0200

    Feat: added sidenav component

---
+++ controllo-ore-x/apps/controllo-ore-x/src/app/_shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'controllo-ore-x-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent {}
