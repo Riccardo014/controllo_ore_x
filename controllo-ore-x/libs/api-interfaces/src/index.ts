7f54d0c05cb3706f562e7331ca5f0abbd1038064
Riccardo Rossi <riky42001@gmail.com>
2023-09-11 16:55:59 +0200

    COX-7 Feat: setup find boosted and crudservice

--- controllo-ore-x/libs/api-interfaces/src/index.ts
+++ controllo-ore-x/libs/api-interfaces/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/api-interfaces';
+export * from './interfaces/find-boosted.interface';
--- controllo-ore-x/libs/find-boosted/src/index.ts
+++ controllo-ore-x/libs/find-boosted/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/find-boosted';
+export * from './lib/find-boosted-result.interface';
