2417031e98b440964c4149dd91500cf061fe6307
Riccardo Rossi <riky42001@gmail.com>
2023-09-12 12:37:39 +0200

    COX-8 setup main.ts

--- controllo-ore-x/apps/api/src/main.ts
+++ controllo-ore-x/apps/api/src/main.ts
@@ -3,15 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger, NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const nestApplicationOptions: NestApplicationOptions = {
+    cors: true,
+  };
+
+  // Remove other logs in production environment
+  if (process.env.NODE_ENV === 'production') {
+    nestApplicationOptions.logger = [
+      'error',
+    ];
+  }
+  
+  const app: INestApplication = await NestFactory.create(AppModule, nestApplicationOptions);
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
