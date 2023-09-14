23f82e29d8c888561d85c30fbc9ce7ebc93c0ef5
Riccardo Rossi <riky42001@gmail.com>
2023-09-14 11:51:21 +0200

    Feat: add eslint and cleaned files

--- apps/api/src/app/modules/user/user.module.ts
+++ apps/api/src/app/modules/user/user.module.ts
@@ -1,15 +1,15 @@
-import { Logger, Module } from "@nestjs/common";
+import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@modules/user/entities/user.entity';
 import { UserService } from '@modules/user/services/user.service';
 import { UserController } from '@modules/user/controllers/user.controller';
-import { ConfigService } from "@nestjs/config";
-import { DataSource } from "typeorm";
-import { USER_SEED } from "./seeds/user.seed";
-import { RoleService } from "@modules/user/services/role.service";
-import { Role } from "@modules/user/entities/role.entity";
-import { ROLE_SEED } from "./seeds/role.seed";
-import { RoleController } from "./controllers/role.controller";
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { USER_SEED } from './seeds/user.seed';
+import { RoleService } from '@modules/user/services/role.service';
+import { Role } from '@modules/user/entities/role.entity';
+import { ROLE_SEED } from './seeds/role.seed';
+import { RoleController } from './controllers/role.controller';
 
 @Module({
   imports: [
