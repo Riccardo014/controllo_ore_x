401556a8ed708cba853de4fbb8981d4256bc7fb4
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 13:01:14 +0200

    Fix: refactor role-create/role-update

--- apps/api/src/app/modules/user/dtov/role-create.dtov.ts
+++ apps/api/src/app/modules/user/dtov/role-create.dtov.ts
@@ -2,7 +2,12 @@ import { RoleCreateDto } from '@api-interfaces';
 import { IsOptional, IsString } from 'class-validator';
 
 export class RoleCreateDtoV implements RoleCreateDto {
-  @IsString() name: string;
-  @IsOptional() @IsString() permissions: string;
+  @IsString() 
+  name: string;
+
+  @IsOptional() 
+  @IsString() 
+  permissions: string;
+
   //I permessi del ruolo sono gestiti tramite un JSON.stringify()
 }
