fdda6f1d5d1ad0726544f90d3e3b94c1a493d0aa
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 12:54:03 +0200

    Fix: fixed user-update

--- apps/api/src/app/modules/user/dtov/user-update.dtov.ts
+++ apps/api/src/app/modules/user/dtov/user-update.dtov.ts
@@ -1,10 +1,23 @@
 import { UserUpdateDto } from '@api-interfaces';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class UserUpdateDtoV implements UserUpdateDto {
-  @IsOptional() @IsEmail() email?: string;
-  @IsOptional() @IsString() roleId?: string;
-  @IsOptional() @IsString() name?: string;
-  @IsOptional() @IsString() surname?: string;
+  @IsOptional() 
+  @IsEmail() 
+  email?: string;
+
+  @IsOptional() 
+  @IsString() 
+  @IsUUID()
+  roleId?: string;
+
+  @IsOptional() 
+  @IsString() 
+  name?: string;
+  
+  @IsOptional() 
+  @IsString() 
+  surname?: string;
+  
   //TODO: avatar
 }
