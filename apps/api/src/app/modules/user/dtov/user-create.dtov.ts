5c6f953ab8134680cfb8491ac1dacabef0f81a16
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 12:52:19 +0200

    Fix: fixed user-create

--- apps/api/src/app/modules/user/dtov/user-create.dtov.ts
+++ apps/api/src/app/modules/user/dtov/user-create.dtov.ts
@@ -1,15 +1,25 @@
 import { UserCreateDto } from '@api-interfaces';
-import { IsOptional, IsString, Matches } from 'class-validator';
+import { IsEmail, IsOptional, IsString, IsUUID, Matches } from 'class-validator';
 
 export class UserCreateDtoV implements UserCreateDto {
-  @IsString() email: string;
+  @IsEmail() 
+  email: string;
+  
   @IsString()
   @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]).{8,}$/, {
     message: 'New password should contain at least 8 characters, including a number, an uppercase letter and a special character',
   })
   password: string;
-  @IsString() roleId: string;
-  @IsString() name: string;
-  @IsString() surname: string;
+
+  @IsString() 
+  @IsUUID() 
+  roleId: string;
+
+  @IsString() 
+  name: string;
+
+  @IsString() 
+  surname: string;
+  
   //TODO: avatar
 }
