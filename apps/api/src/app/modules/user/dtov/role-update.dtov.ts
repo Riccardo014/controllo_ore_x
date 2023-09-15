81ff85f4bac7c09930e02dea1538d7f78704b914
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 12:42:33 +0200

    Fix: fixed role name

--- apps/api/src/app/modules/user/dtov/role-update.dtov.ts
+++ apps/api/src/app/modules/user/dtov/role-update.dtov.ts
@@ -3,4 +3,5 @@ import { IsOptional, IsString } from 'class-validator';
 
 export class RoleUpdateDtoV implements RoleUpdateDto {
   @IsOptional() @IsString() permissions?: string;
+  @IsOptional() @IsString() name?: string;
 }
