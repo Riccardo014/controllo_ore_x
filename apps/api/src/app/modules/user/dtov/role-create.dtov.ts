9234103be9899ad592aca78ae4b2ffb2c857f390
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 12:40:15 +0200

    Fix: fixed role-create.dtov

--- apps/api/src/app/modules/user/dtov/role-create.dtov.ts
+++ apps/api/src/app/modules/user/dtov/role-create.dtov.ts
@@ -4,4 +4,5 @@ import { IsOptional, IsString } from 'class-validator';
 export class RoleCreateDtoV implements RoleCreateDto {
   @IsString() name: string;
   @IsOptional() @IsString() permissions: string;
+  //I permessi del ruolo sono gestiti tramite un JSON.stringify()
 }
