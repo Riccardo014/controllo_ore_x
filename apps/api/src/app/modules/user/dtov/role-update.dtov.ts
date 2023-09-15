9339c9f4b4cd1fa1cde371f090663d27a8bcb1cf
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-15 13:01:14 +0200

    Fix: refactor role-create/role-update

--- apps/api/src/app/modules/user/dtov/role-update.dtov.ts
+++ apps/api/src/app/modules/user/dtov/role-update.dtov.ts
@@ -2,6 +2,12 @@ import { RoleUpdateDto } from '@api-interfaces';
 import { IsOptional, IsString } from 'class-validator';
 
 export class RoleUpdateDtoV implements RoleUpdateDto {
-  @IsOptional() @IsString() permissions?: string;
-  @IsOptional() @IsString() name?: string;
+  @IsOptional() 
+  @IsString() 
+  permissions?: string;
+  
+  @IsOptional() 
+  @IsString() 
+  name?: string;
+
 }
