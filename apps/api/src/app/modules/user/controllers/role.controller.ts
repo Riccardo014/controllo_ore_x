618a52558602fd8b1f0286f0b85318083c24cd45
Riccardo Rossi <riky42001@gmail.com>
2023-09-18 14:07:27 +0200

    Feat: added auth user and role checker

--- apps/api/src/app/modules/user/controllers/role.controller.ts
+++ apps/api/src/app/modules/user/controllers/role.controller.ts
@@ -1,17 +1,21 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CastObjectPipe } from '@shared/pipes/cast-object.pipe';
 import { ApiTags } from '@nestjs/swagger';
-import { FindBoostedOptions } from '@api-interfaces';
+import { FindBoostedOptions, ROLE } from '@api-interfaces';
 import { FindBoostedResult } from '@find-boosted';
 import { UpdateResult } from 'typeorm';
 import { Role } from '../entities/role.entity';
 import { RoleUpdateDtoV } from '../dtov/role-update.dtov';
 import { RoleService } from '../services/role.service';
+import { AuthUser } from '@shared/decorators/auth-user.decorator';
+import { User } from '../entities/user.entity';
+import { RoleChecker } from '@shared/utils/role-checker';
+import { UserService } from '../services/user.service';
 
 @ApiTags('Roles')
 @Controller('roles')
 export class RoleController {
-  constructor(private _roleService: RoleService) {
+  constructor(private _roleService: RoleService, private _userService: UserService) {
   }
 
   @Get()
@@ -32,6 +36,15 @@ export class RoleController {
   @Put(':id')
   update(@Param('id') id: string, @Body() body: RoleUpdateDtoV): Promise<UpdateResult> {
     return this._roleService.update(id, body);
+  }
+
+  @Get('roleId/:name')
+  getRoleIdByName(@Param('name') name: string, @AuthUser() user: User): string {
+    const permittedRoles = [ROLE.SUPERADMIN, ROLE.ADMIN, ROLE.COLLABORATOR];
+    if(!RoleChecker.checkPermission(user, this._userService, permittedRoles)){
+      throw new ForbiddenException();
+    }
+    return 'All good';
   }
 
 }
--- libs/api-interfaces/src/index.ts
+++ libs/api-interfaces/src/index.ts
@@ -2,5 +2,7 @@ export * from './interfaces/base-response.interface';
 export * from './interfaces/find-boosted.interface';
 
 export * from './lib/api-interfaces';
-export * from "./dto/user/user.dto";
-export * from "./dto/user/role.dto";
+export * from './dto/user/user.dto';
+export * from './dto/user/role.dto';
+
+export * from './enums/role.enum';
