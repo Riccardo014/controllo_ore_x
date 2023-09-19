078616b01fe23ea76c3e91fb958bf3a930a7383b
Riccardo Rossi <riky42001@gmail.com>
2023-09-19 16:38:28 +0200

    Fix: timestamp from class to interface

--- apps/api/src/app/_shared/classes/timestamps-entity.class.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export abstract class TimestampsEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
