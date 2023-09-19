8007c41acd8ecd79bfe8170c7f01e03a22161992
Riccardo Rossi <riky42001@gmail.com>
2023-09-19 10:23:33 +0200

    Feat: added error handling for rolechecker

--- apps/api/src/app/_shared/utils/role-checker.ts
+++ apps/api/src/app/_shared/utils/role-checker.ts
@@ -1,7 +1,7 @@
 import { User } from '@modules/user/entities/user.entity';
 import { UserService } from '@modules/user/services/user.service';
 import { ROLE } from '@api-interfaces';
-import { ForbiddenException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException } from '@nestjs/common';
 
 export abstract class RoleChecker {
   
@@ -11,15 +11,18 @@ export abstract class RoleChecker {
    * @param _userService The user service
    * @param roles The permitted roles
    * @throws ForbiddenException if the user role is not in the permitted roles
-   * @returns True if the role of the user is in the permitted roles
+   * @returns True if the role of the user is in the permitted roles false otherwise
    */
   static async checkPermission(user: User, _userService: UserService, roles: ROLE[]): Promise<boolean> {
+      const currentUser = await _userService.getOne( user._id, [ 'role' ] );
 
-    const currentUserRole = (await _userService.getOne( user._id, [ 'role' ] )).role;
-    if (!roles.includes(currentUserRole.name as ROLE)) {
-      throw new ForbiddenException();
-    }
-    return true;
+      if (!currentUser) {
+        throw new BadRequestException();
+      }
+      if (!roles.includes(currentUser.role.name as ROLE)) {
+        throw new ForbiddenException();
+      }
+      return true;
   }
 
 }
