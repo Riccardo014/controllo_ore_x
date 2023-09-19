cbfa842a77237934ad008d8d8f26b1d7a70b1266
Riccardo Rossi <riky42001@gmail.com>
2023-09-19 12:34:39 +0200

    Fix: fixed 002 message

--- apps/api/src/app/_shared/utils/errors/api-errors.ts
+++ apps/api/src/app/_shared/utils/errors/api-errors.ts
@@ -9,7 +9,7 @@ export class ApiErrors {
     static readonly MISSING_USER_DATA: IErrorBody = {
         code: '002',
         scope: 'General',
-        message: 'Si è verificato un errore durante il recupero dei deti dell\'utente'
+        message: 'Si è verificato un errore durante il recupero dei dati dell\'utente'
     };
     static readonly WRONG_PASSWORD: IErrorBody = {
         code: '003',
