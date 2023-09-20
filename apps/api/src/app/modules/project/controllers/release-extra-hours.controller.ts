3e8a1503a8c92c552b248f86f5e511da5ebc8c2a
Riccardo Rossi <riky42001@gmail.com>
2023-09-20 16:04:05 +0200

    Fix: changed module name

--- apps/api/src/app/modules/project/controllers/release-extra-hours.controller.ts
+++ apps/api/src/app/modules/project/controllers/release-extra-hours.controller.ts
@@ -4,53 +4,53 @@ import { AuthUser } from '@shared/decorators/auth-user.decorator';
 import { ApiTags } from '@nestjs/swagger';
 import { FindBoostedOptions } from '@api-interfaces';
 import { FindBoostedResult } from '@find-boosted';
-import { HoursExtraService } from '@modules/project/services/hours-extra.service';
-import { HoursExtraCreateDtoV } from '@modules/project/dtov/hours-extra-create.dtov';
-import { HoursExtraUpdateDtoV } from '@modules/project/dtov/hours-extra-update.dtov';
+import { ReleaseExtraHoursService } from '@modules/project/services/release-extra-hours.service';
+import { ReleaseExtraHoursCreateDtoV } from '@modules/project/dtov/release-extra-hours-create.dtov';
+import { ReleaseExtraHoursUpdateDtoV } from '@modules/project/dtov/release-extra-hours-update.dtov';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { User } from '@modules/user/entities/user.entity';
 import { UserService } from '@modules/user/services/user.service';
-import { HoursExtra } from '../entities/hours-extra.entity';
+import { ReleaseExtraHours } from '../entities/release-extra-hours.entity';
 import { ApiErrors } from '@shared/utils/errors/api-errors';
 import { RoleChecker } from '@shared/utils/role-checker';
 
-@ApiTags('HoursExtras')
-@Controller('hoursExtras')
-export class HoursExtraController {
-  constructor(private _hoursExtraService: HoursExtraService, private _userService: UserService) {
+@ApiTags('ReleaseExtraHours')
+@Controller('releaseExtraHours')
+export class ReleaseExtraHoursController {
+  constructor(private _releaseExtraHoursService: ReleaseExtraHoursService, private _userService: UserService) {
   }
 
   @Get()
-  getMany(@Query(CastObjectPipe) query: any): Promise<FindBoostedResult<HoursExtra>> {
-    return this._hoursExtraService.getMany(query);
+  getMany(@Query(CastObjectPipe) query: any): Promise<FindBoostedResult<ReleaseExtraHours>> {
+    return this._releaseExtraHoursService.getMany(query);
   }
 
   @Post('fb')
-  getManyFb(@Body() body: FindBoostedOptions): Promise<FindBoostedResult<HoursExtra>> {
-    return this._hoursExtraService.getMany(body);
+  getManyFb(@Body() body: FindBoostedOptions): Promise<FindBoostedResult<ReleaseExtraHours>> {
+    return this._releaseExtraHoursService.getMany(body);
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): Promise<HoursExtra> {
-    return this._hoursExtraService.getOne(id);
+  getOne(@Param('id') id: string): Promise<ReleaseExtraHours> {
+    return this._releaseExtraHoursService.getOne(id);
   }
 
   @Post()
-  create(@Body() data: HoursExtraCreateDtoV, @AuthUser() user: User): Promise<HoursExtra> {
+  create(@Body() data: ReleaseExtraHoursCreateDtoV, @AuthUser() user: User): Promise<ReleaseExtraHours> {
     if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
-    return this._hoursExtraService.create(data);
+    return this._releaseExtraHoursService.create(data);
   }
 
   @Put(':id')
   update(@Param('id') id: string,
-         @Body() body: HoursExtraUpdateDtoV,
+         @Body() body: ReleaseExtraHoursUpdateDtoV,
          @AuthUser() user: User): Promise<UpdateResult> {
     if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
-    return this._hoursExtraService.update(id, body);
+    return this._releaseExtraHoursService.update(id, body);
   }
 
   @Delete(':id')
@@ -58,6 +58,6 @@ export class HoursExtraController {
     if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
-    return this._hoursExtraService.delete({ _id: id });
+    return this._releaseExtraHoursService.delete({ _id: id });
   }
 }
--- apps/api/src/app/modules/project/project.module.ts
+++ apps/api/src/app/modules/project/project.module.ts
@@ -8,27 +8,23 @@ import { UserService } from '@modules/user/services/user.service';
 import { HoursExtraController } from '@modules/project/controllers/hours-extra.controller';
 import { HoursExtraService } from '@modules/project/services/hours-extra.service';
 import { HoursExtra } from '@modules/project/entities/hours-extra.entity';
+import { ReleaseExtraHoursController } from '@modules/project/controllers/release-extra-hours.controller';
+import { ReleaseExtraHoursService } from '@modules/project/services/release-extra-hours.service';
+import { ReleaseExtraHours } from '@modules/project/entities/release-extra-hours.entity';
+import { UserModule } from '@modules/user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      Project,
-      HoursExtra,
-        ]),
-    UserModule
+    TypeOrmModule.forFeature([Project, HoursExtra, ReleaseExtraHours]),
+    UserModule,
   ],
-  controllers: [
-    ProjectController,
-    HoursExtraController,
-],
-  exports: [
-    ProjectService,
-    HoursExtraService,
-],
+  controllers: [ProjectController, ReleaseExtraHoursController],
+  exports: [ProjectService, ReleaseExtraHoursService],
   providers: [
     ProjectService,
     UserService,
     HoursExtraService,
-]
+    ReleaseExtraHoursService,
+  ],
 })
 export class ProjectModule {}
