4c34d4601c0f1ae640590d2be6b1ac13a5f50f07
Riccardo Rossi <riky42001@gmail.com>
2023-09-21 08:58:27 +0200

    Feat: added user hours module

--- apps/api/src/app/modules/user-hours/dtov/user-hours-update.dtov.ts
+++ apps/api/src/app/modules/user-hours/dtov/user-hours-update.dtov.ts
@@ -1,5 +1,28 @@
 import { UserHoursUpdateDto } from '@api-interfaces';
+import { IsDate, IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class UserHoursUpdateDtoV implements UserHoursUpdateDto {
+  @IsOptional() 
+  @IsUUID()
+  userId?: string;
 
+  @IsOptional() 
+  @IsUUID()
+  releaseId?: string;
+
+  @IsOptional() 
+  @IsUUID()
+  labelId?: string;
+
+  @IsOptional() 
+  @IsDate()
+  date?: Date;
+
+  @IsOptional() 
+  @IsString()
+  notes?: string;
+
+  @IsOptional() 
+  @IsNumber()
+  hours?: number;
 }
