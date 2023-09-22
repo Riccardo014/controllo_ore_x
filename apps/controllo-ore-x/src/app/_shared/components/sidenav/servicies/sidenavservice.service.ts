cad09a5d33ece9a9be9023173a1cf8125a864c07
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-22 12:58:46 +0200

    Feat: added sidenav service

---
+++ apps/controllo-ore-x/src/app/_shared/components/sidenav/servicies/sidenavservice.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidenavService {
+
+  sidebarOpen: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  currentSidebarStatus: Observable<boolean> = this.sidebarOpen.asObservable();
+
+  toggleSidebar(): void {
+    this.sidebarOpen.next(!this.sidebarOpen.value);
+  }
+  
+}
