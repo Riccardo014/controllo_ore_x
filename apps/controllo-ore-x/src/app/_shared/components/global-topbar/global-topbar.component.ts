db4bd6ec940a4d29d9c3ff11cb342e7d1a2dd3bb
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-09-22 12:57:22 +0200

    Feat: updated global topbar

--- apps/controllo-ore-x/src/app/_shared/components/global-topbar/global-topbar.component.ts
+++ apps/controllo-ore-x/src/app/_shared/components/global-topbar/global-topbar.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidenavService } from '../sidenav/servicies/sidenavservice.service';
 
 @Component({
   selector: 'controllo-ore-x-global-topbar',
   templateUrl: './global-topbar.component.html',
   styleUrls: ['./global-topbar.component.scss'],
 })
-export class GlobalTopbarComponent {
+export class GlobalTopbarComponent implements OnInit{
 
-  current_time = "11:20";
+  current_time = '11:20';
 
+  isSidenavOpen: boolean = true;
 
-  @Input() isMenuButtonShown: boolean = true;
+  constructor(private _sidenavService: SidenavService) {}
+
+  ngOnInit(): void {
+    this._sidenavService.currentSidebarStatus.subscribe(isOpen => this.isSidenavOpen = isOpen);
+  }
 
   toggleSidebar(): void {
-
+    this._sidenavService.toggleSidebar();
   }
 
 }
