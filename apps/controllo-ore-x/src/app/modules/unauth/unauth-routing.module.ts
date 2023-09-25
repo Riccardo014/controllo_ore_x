264e2137a920e4968b9d3b7cff30bbde6cd7deb8
Riccardo Rossi <riky42001@gmail.com>
2023-09-25 14:10:01 +0200

    Fix: fixed unauth logic

--- apps/controllo-ore-x/src/app/modules/unauth/unauth-routing.module.ts
+++ apps/controllo-ore-x/src/app/modules/unauth/unauth-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
 
-const screenWidth: number = window.innerWidth;
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: screenWidth > 768 ? 'accedi' : 'accedi',
+    redirectTo: 'accedi',
     //TODO: scherata per ipad e cellulare
     //redirectTo: screenWidth > 768 ? 'accedi' : 'benvenuto',
   },
