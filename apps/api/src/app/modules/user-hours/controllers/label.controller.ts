acf42af5eddc0d02af225d291ca0a0c2b9f8742b
Riccardo Rossi <riky42001@gmail.com>
2023-10-03 08:45:47 +0200

    Refactor: refactored role-checker

--- apps/api/src/app/modules/user-hours/controllers/label.controller.ts
+++ apps/api/src/app/modules/user-hours/controllers/label.controller.ts
@@ -1,18 +1,28 @@
-import { Body, Controller, Delete, ForbiddenException, Get, Param, Post, Put, Query } from '@nestjs/common';
-import { CastObjectPipe } from '@shared/pipes/cast-object.pipe';
-import { AuthUser } from '@shared/decorators/auth-user.decorator';
-import { ApiTags } from '@nestjs/swagger';
 import { FindBoostedOptions } from '@api-interfaces';
 import { FindBoostedResult } from '@find-boosted';
-import { LabelService } from '@modules/user-hours/services/label.service';
 import { LabelCreateDtoV } from '@modules/user-hours/dtov/label-create.dtov';
 import { LabelUpdateDtoV } from '@modules/user-hours/dtov/label-update.dtov';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { LabelService } from '@modules/user-hours/services/label.service';
 import { User } from '@modules/user/entities/user.entity';
-import { Label } from '../entities/label.entity';
 import { UserService } from '@modules/user/services/user.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  ForbiddenException,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthUser } from '@shared/decorators/auth-user.decorator';
+import { CastObjectPipe } from '@shared/pipes/cast-object.pipe';
 import { ApiErrors } from '@shared/utils/errors/api-errors';
 import { RoleChecker } from '@shared/utils/role-checker';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { Label } from '../entities/label.entity';
 
 /**
  * @description The labels are used to categorize the user hours.
@@ -20,16 +30,22 @@ import { RoleChecker } from '@shared/utils/role-checker';
 @ApiTags('Labels')
 @Controller('labels')
 export class LabelController {
-  constructor(private _labelService: LabelService, private _userService: UserService) {
-  }
+  constructor(
+    private _labelService: LabelService,
+    private _userService: UserService,
+  ) {}
 
   @Get()
-  getMany(@Query(CastObjectPipe) query: any): Promise<FindBoostedResult<Label>> {
+  getMany(
+    @Query(CastObjectPipe) query: any,
+  ): Promise<FindBoostedResult<Label>> {
     return this._labelService.getMany(query);
   }
 
   @Post('fb')
-  getManyFb(@Body() body: FindBoostedOptions): Promise<FindBoostedResult<Label>> {
+  getManyFb(
+    @Body() body: FindBoostedOptions,
+  ): Promise<FindBoostedResult<Label>> {
     return this._labelService.getMany(body);
   }
 
@@ -39,26 +55,34 @@ export class LabelController {
   }
 
   @Post()
-  create(@Body() data: LabelCreateDtoV, @AuthUser() user: User): Promise<Label> {
-    if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
+  create(
+    @Body() data: LabelCreateDtoV,
+    @AuthUser() user: User,
+  ): Promise<Label> {
+    if (!RoleChecker.isUserRoleAdminOrHigher(user, this._userService)) {
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
     return this._labelService.create(data);
   }
 
   @Put(':id')
-  update(@Param('id') id: string,
-         @Body() body: LabelUpdateDtoV,
-         @AuthUser() user: User): Promise<UpdateResult> {
-    if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
+  update(
+    @Param('id') id: string,
+    @Body() body: LabelUpdateDtoV,
+    @AuthUser() user: User,
+  ): Promise<UpdateResult> {
+    if (!RoleChecker.isUserRoleAdminOrHigher(user, this._userService)) {
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
     return this._labelService.update(id, body);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string, @AuthUser() user: User): Promise<DeleteResult> {
-    if(!RoleChecker.userRoleIsAdminOrHigher(user, this._userService)){
+  delete(
+    @Param('id') id: string,
+    @AuthUser() user: User,
+  ): Promise<DeleteResult> {
+    if (!RoleChecker.isUserRoleAdminOrHigher(user, this._userService)) {
       throw new ForbiddenException(ApiErrors.UNUTHORIZED_OPERATION);
     }
     return this._labelService.delete({ _id: id });
