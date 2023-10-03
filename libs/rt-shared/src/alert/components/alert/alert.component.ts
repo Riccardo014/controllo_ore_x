83e3917764b35785099ab3a669c8327e0a21c1be
Riccardo Rossi <riky42001@gmail.com>
2023-10-03 16:57:41 +0200

    Feat: updated alert component

--- libs/rt-shared/src/alert/components/alert/alert.component.ts
+++ libs/rt-shared/src/alert/components/alert/alert.component.ts
@@ -1,42 +1,86 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription, interval, timeout } from 'rxjs';
+import {
+  SubscriptionsLifecycle,
+  completeSubscriptions,
+} from '../../../../../../apps/controllo-ore-x/src/app/utils/subscriptions_lifecycle';
+import { AlertService } from '../../services/alert.service';
 import { RtAlert } from './alert.interface';
-import { AlertService } from "../../services/alert.service";
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'lib-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss',],
+  styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent implements OnInit {
-  static readonly ALERT_TIMEOUT: number = 5000;
+export class AlertComponent implements OnInit, SubscriptionsLifecycle {
+  private readonly _ALERT_TIMEOUT: number = 5000;
   @Input() alert!: RtAlert;
 
-  currentPosition: number = AlertComponent.ALERT_TIMEOUT;
-  total: number = AlertComponent.ALERT_TIMEOUT;
+  /**
+   * How much time passed since the alert was created.
+   */
+  private _alertLifetimeElapsed: number = 0;
+
+  /**
+   * The total lifetime of the alert.
+   */
+  private _alertLifetimeTotal: number = this._ALERT_TIMEOUT;
+
+  subscriptionsList: Subscription[] = [];
+
+  _completeSubscriptions: (subscriptionsList: Subscription[]) => void =
+    completeSubscriptions;
 
   currentTimeout: any;
   currentInterval: any;
   isPinned: boolean = false;
   shouldShowDetails: boolean = false;
 
-  constructor(private _rtAlertSvc: AlertService) {
-  }
+  constructor(private _rtAlertSvc: AlertService) {}
 
   ngOnInit(): void {
-    this.currentTimeout = setTimeout(() => {
-      this.close();
-    }, AlertComponent.ALERT_TIMEOUT);
-
-    this.currentInterval = setInterval(() => {
-      this.currentPosition -= 100;
-    }, 100);
+    this._setSubscriptions();
   }
 
+  ngOnDestroy(): void {
+    this._completeSubscriptions(this.subscriptionsList);
+  }
+
+  _setSubscriptions(): void {
+    this.subscriptionsList.push(this.setTimeout());
+    this.subscriptionsList.push(this.setInterval());
+  }
+
+  /**
+   * Set the timeout for the alert lifetime
+   */
+  setTimeout(): Subscription {
+    return interval(this._ALERT_TIMEOUT)
+      .pipe(timeout(this._ALERT_TIMEOUT))
+      .subscribe((_) => {
+        this.close();
+      });
+  }
+
+  /**
+   * Set the interval for the alert lifetime progress bar
+   */
+  setInterval(): Subscription {
+    return interval(100)
+      .pipe()
+      .subscribe((_) => {
+        this._alertLifetimeElapsed -= 100;
+      });
+  }
+
+  /**
+   * If the alert is pinned, the interval and the timeout are cleared.
+   */
   pin(): void {
     this.isPinned = true;
     clearInterval(this.currentInterval);
     clearTimeout(this.currentTimeout);
+    this._completeSubscriptions(this.subscriptionsList);
   }
 
   close(): void {
@@ -47,5 +91,9 @@ export class AlertComponent implements OnInit {
 
   toggleDetails(): void {
     this.shouldShowDetails = !this.shouldShowDetails;
+  }
+
+  getLifetimeProgress(): number {
+    return (this._alertLifetimeElapsed / this._alertLifetimeTotal) * 100;
   }
 }
