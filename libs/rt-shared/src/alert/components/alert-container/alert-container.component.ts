65d77ccdb947c87e08044878ad58f86507730dc8
Riccardo Rossi <riky42001@gmail.com>
2023-10-03 16:13:23 +0200

    Feat: updated alert container component

--- libs/rt-shared/src/alert/components/alert-container/alert-container.component.ts
+++ libs/rt-shared/src/alert/components/alert-container/alert-container.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { RtAlert } from "../alert/alert.interface";
-import { AlertService } from "../../services/alert.service";
-
+import { AlertService } from '../../services/alert.service';
+import { RtAlert } from '../alert/alert.interface';
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'lib-alert-container',
   templateUrl: './alert-container.component.html',
-  styleUrls: ['./alert-container.component.scss',],
+  styleUrls: ['./alert-container.component.scss'],
 })
 export class AlertContainerComponent implements OnInit {
-  currentAlerts: RtAlert[] = [];
+  alerts: RtAlert[] = [];
 
-  constructor(private _rtAlert: AlertService) {
-  }
+  constructor(private _rtAlert: AlertService) {}
 
   ngOnInit(): void {
-    this._rtAlert.alerts.subscribe(r => {
-      this.currentAlerts = r;
+    this._rtAlert.alerts.subscribe((alert) => {
+      this.alerts = alert;
     });
   }
-
 }
