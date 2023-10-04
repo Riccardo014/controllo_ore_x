4fa72d3f0a3357662218bc4c0bd105c984d1e5ec
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 12:20:24 +0200

    Feat: updated rt table td component

--- libs/rt-shared/src/rt-table/components/rt-table-td/rt-table-td.component.ts
+++ libs/rt-shared/src/rt-table/components/rt-table-td/rt-table-td.component.ts
@@ -8,7 +8,7 @@ import { rtTableColumnValue } from '../../lib/rt-table-column-value';
   styleUrls: ['./rt-table-td.component.scss'],
 })
 export class RtTableTdComponent implements OnInit {
-  @Input() column!: TableConfigurationColumn;
+  @Input() column?: TableConfigurationColumn;
   @Input() entity: any;
 
   @Output() openDialog: EventEmitter<any> = new EventEmitter<any>();
@@ -17,16 +17,18 @@ export class RtTableTdComponent implements OnInit {
 
   roles: ROLE[] = [ROLE.SUPERADMIN, ROLE.ADMIN, ROLE.COLLABORATOR];
 
-  // PAYMENT_STATUS: typeof PAYMENT_STATUS = PAYMENT_STATUS;
-
-  // REQUEST_STATUS: typeof REQUEST_STATUS = REQUEST_STATUS;
+  constructor() {
+    if (!this.column) {
+      throw new Error('column is undefined');
+    }
+  }
 
   ngOnInit(): void {
     this.valueToPrint = this.getColumnData();
   }
 
   getColumnData(): any {
-    return rtTableColumnValue(this.column, this.entity);
+    return rtTableColumnValue(this.column!, this.entity);
   }
 
   openDialogFn(entity: any): void {
