6a3e500eb77e099b8d8bfbdb69f5267ddd4d16ef
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 10:53:23 +0200

    Feat: updated base data service

--- libs/rt-shared/src/index.ts
+++ libs/rt-shared/src/index.ts
@@ -3,10 +3,10 @@ export * from './alert/alert.module';
 export * from './rt-dialog/rt-dialog.module';
 export * from './rt-input/rt-input.module';
 export * from './rt-loading/rt-loading.module';
-export * from './rt-table/rt-table.module';
 export * from './rt-select/rt-select.module';
+export * from './rt-table/rt-table.module';
 
 // Classes
-export * from './rt-table/classes/data-service';
+export * from './rt-table/classes/base-data-service.class';
 export * from './rt-table/classes/rt-table-api-status-manager';
 export * from './rt-table/classes/rt-table-status-handler';
--- libs/rt-shared/src/rt-table/index.ts
+++ libs/rt-shared/src/rt-table/index.ts
@@ -1,7 +1,6 @@
-export * from './classes/data-service';
+export * from './classes/base-data-service.class';
 export * from './classes/rt-table-api-status-manager';
 export * from './classes/rt-table-status-handler';
 export * from './interfaces/rt-table-status.interface';
 export * from './lib/rt-table-column-value';
 export * from './rt-table.module';
-
