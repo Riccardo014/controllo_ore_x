e579daead3138c24e9873d8024f505c5d18a5c2e
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 08:57:47 +0200

    Feat: updated rt dialog defailt action component

--- libs/rt-shared/src/rt-dialog/components/rt-dialog-default-actions/rt-dialog-default-actions.component.ts
+++ libs/rt-shared/src/rt-dialog/components/rt-dialog-default-actions/rt-dialog-default-actions.component.ts
@@ -1,31 +1,17 @@
-/**
- * @version 1.0.0
- *
- * Snippet Name: RT Dialog Default Actions Component
- * Summary: Angular component to use inside dialogTemplate
- * Description: used inside RtDialogTemplate to display a basic footer, with basic actions
- *
- * File Changelog
- *
- * Author                       | Date            | Changes
- * =====================================================================================================================
- * Luca Bertolini               | 02/04/2021      | Initial Import
- */
-
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+/**
+ * Display a basic footer, with basic actions, in a RtDialogTemplate
+ */
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'rt-dialog-default-actions',
   templateUrl: './rt-dialog-default-actions.component.html',
-  styleUrls: [
-    './rt-dialog-default-actions.component.scss',
-  ],
+  styleUrls: ['./rt-dialog-default-actions.component.scss'],
 })
 export class RtDialogDefaultActionsComponent {
   @Input() textConfirm: string = 'Ok';
   @Input() textCancel: string = 'Annulla';
 
-  @Output() confirm: EventEmitter<void> = new EventEmitter<void>();
-  @Output() cancel: EventEmitter<void> = new EventEmitter<void>();
+  @Output() confirmEmitter: EventEmitter<void> = new EventEmitter<void>();
+  @Output() cancelEmitter: EventEmitter<void> = new EventEmitter<void>();
 }
