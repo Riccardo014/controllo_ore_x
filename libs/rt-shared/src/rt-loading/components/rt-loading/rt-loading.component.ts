27dfece7c7b92fa9b1b3838a112b07a1583ce6e7
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 10:18:40 +0200

    Feat: updated rt loading component

--- libs/rt-shared/src/rt-loading/components/rt-loading/rt-loading.component.ts
+++ libs/rt-shared/src/rt-loading/components/rt-loading/rt-loading.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 import { RtLoadingService } from '../../services/rt-loading.service';
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'lib-rt-loading',
   templateUrl: './rt-loading.component.html',
-  styleUrls: [
-    './rt-loading.component.scss',
-  ],
+  styleUrls: ['./rt-loading.component.scss'],
 })
 export class RtLoadingComponent {
-  constructor(public loadingSvc: RtLoadingService) { }
+  constructor(public loadingSvc: RtLoadingService) {}
 }
-
