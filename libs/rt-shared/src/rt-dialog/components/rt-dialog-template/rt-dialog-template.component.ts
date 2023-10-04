5bd312d9e8806dd2f53d3554c7c4ba6960d2ae81
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 08:59:37 +0200

    Feat: updated rt dialog template component

--- libs/rt-shared/src/rt-dialog/components/rt-dialog-template/rt-dialog-template.component.ts
+++ libs/rt-shared/src/rt-dialog/components/rt-dialog-template/rt-dialog-template.component.ts
@@ -1,19 +1,8 @@
-/**
- * @version 1.0.0
- *
- * Snippet Name: RT Dialog Component
- * Summary: Angular component
- * Description: used by RtDialogService to set the template of all the dialogs
- *
- * File Changelog
- *
- * Author                       | Date            | Changes
- * =====================================================================================================================
- * Luca Bertolini               | 02/04/2021      | Initial Import
- */
-
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+/**
+ * Set the template of all the dialogs, it's used in RtDialogService
+ */
 @Component({
   selector: 'cox-dialog-template',
   templateUrl: './rt-dialog-template.component.html',
@@ -29,5 +18,5 @@ export class RtDialogTemplateComponent {
   @Input() title: string | undefined;
   @Input() subtitle: string | undefined;
 
-  @Output() reFetch: EventEmitter<void> = new EventEmitter<void>();
+  @Output() reFetchEmitter: EventEmitter<void> = new EventEmitter<void>();
 }
