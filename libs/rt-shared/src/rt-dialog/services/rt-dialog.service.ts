730a6e10c36ffb7886c9edf0ca0136be0ca7fc49
Riccardo Rossi <riky42001@gmail.com>
2023-10-04 17:30:18 +0200

    Refactor: removed unused method rt dialog service

--- libs/rt-shared/src/rt-dialog/services/rt-dialog.service.ts
+++ libs/rt-shared/src/rt-dialog/services/rt-dialog.service.ts
@@ -1,10 +1,5 @@
-import { ComponentType } from '@angular/cdk/portal';
-import { Injectable, TemplateRef } from '@angular/core';
-import {
-  MatDialog,
-  MatDialogConfig,
-  MatDialogRef,
-} from '@angular/material/dialog';
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { RtDialogConfirmationComponent } from '../components/rt-dialog-confirmation/rt-dialog-confirmation.component';
 import { IRtDialogClose } from '../interfaces/i-rt-dialog-close.interface';
@@ -17,35 +12,6 @@ export class RtDialogService {
   DEFAULT_WIDTH = '400px';
   CONFERMATION_WIDTH = '500px';
   constructor(private _matDialogSvc: MatDialog) {}
-
-  /**
-   * Open a dialog. If some of the config properties are left empty, defaults are used.
-   *
-   * @param title of the dialog
-   * @param component that is used to display the dialog
-   * @param config MatDialogConfig
-   */
-  open<TInput = any, TReturn = any>(
-    title: string,
-    component: ComponentType<any> | TemplateRef<any>,
-    config: MatDialogConfig<TInput> = {},
-  ): Observable<IRtDialogClose<TReturn>> {
-    // Set the dialog's width.
-    config.width = config?.width ?? this.DEFAULT_WIDTH;
-
-    // Disable closing the dialog by clicking outside of it or by pressing ESC.
-    config.disableClose = config?.disableClose ?? true;
-
-    const dialogRef: MatDialogRef<ComponentType<any> | TemplateRef<any>> =
-      this._matDialogSvc.open(component, {
-        ...config,
-        data: {
-          input: config.data,
-          title,
-        },
-      });
-    return dialogRef.afterClosed() as Observable<IRtDialogClose<TReturn>>;
-  }
 
   openConfirmation(title: string, input?: string): Observable<IRtDialogClose> {
     const dialogRef: MatDialogRef<RtDialogConfirmationComponent> =
