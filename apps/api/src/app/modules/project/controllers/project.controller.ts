3b512d881433a2c80d838578f4f7fce78f4c70de
Riccardo Rossi <riky42001@gmail.com>
2023-10-05 12:55:32 +0200

    Feat: updated project controller

--- apps/api/src/app/modules/project/controllers/project.controller.ts
+++ apps/api/src/app/modules/project/controllers/project.controller.ts
@@ -48,7 +48,7 @@ export class ProjectController {
 
   @Get(':id')
   getOne(@Param('id') id: string): Promise<Project> {
-    return this._projectService.getOne(id);
+    return this._projectService.getOne(id, ['customer']);
   }
 
   @Post()
