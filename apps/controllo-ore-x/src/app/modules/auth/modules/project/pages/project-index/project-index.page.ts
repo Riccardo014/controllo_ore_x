3982a85890a30d40bd47cad3353a9ea7418661fc
Riccardo Rossi <riky42001@gmail.com>
2023-10-05 12:59:30 +0200

    Feat: added project index page and component

---
+++ apps/controllo-ore-x/src/app/modules/auth/modules/project/pages/project-index/project-index.page.ts
@@ -0,0 +1,112 @@
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  ApiPaginatedResponse,
+  INDEX_CONFIGURATION_KEY,
+  ProjectCreateDto,
+  ProjectReadDto,
+  ProjectUpdateDto,
+} from '@api-interfaces';
+import { CustomerDataService } from '@app/_core/services/customer.data-service';
+import { IndexConfigurationDataService } from '@app/_core/services/index-configuration.data-service';
+import { ProjectDataService } from '@app/_core/services/project.data-service';
+import { IndexPage } from '@app/_shared/classes/index-page.class';
+import {
+  SubscriptionsLifecycle,
+  completeSubscriptions,
+} from '@app/utils/subscriptions_lifecycle';
+import { RtDialogService } from 'libs/rt-shared/src/rt-dialog/services/rt-dialog.service';
+import { RtLoadingService } from 'libs/rt-shared/src/rt-loading/services/rt-loading.service';
+import { ProjectWithCustomer } from 'libs/utils/controllo-ore-x-interfaces/projectWithCustomer';
+import { BehaviorSubject, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'controllo-ore-x-project-index',
+  templateUrl: './project-index.page.html',
+  styleUrls: ['./project-index.page.scss'],
+})
+export class ProjectIndexPage
+  extends IndexPage<ProjectReadDto, ProjectCreateDto, ProjectUpdateDto>
+  implements OnInit, OnDestroy, SubscriptionsLifecycle
+{
+  titleIcon: string | null = 'workspaces';
+  title: string = 'Progetti';
+  pageTitle = 'Progetti';
+  buttonIcon = 'egg';
+  buttonText = 'Nuovo Progetto';
+
+  CONFIGURATION_KEY: INDEX_CONFIGURATION_KEY = INDEX_CONFIGURATION_KEY.PROJECT;
+  isItLoading: boolean = false;
+  _isFirstLoadDone: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false,
+  );
+  hasErrors: boolean = false;
+  isEditAvailable: boolean = false;
+  override isPageWithTable: boolean = false;
+
+  projects: ProjectReadDto[] = [];
+  projectsWithCustomer: ProjectWithCustomer[] = [];
+
+  @Output() openDialog: EventEmitter<any> = new EventEmitter<any>();
+
+  override subscriptionsList: Subscription[] = [];
+
+  override _completeSubscriptions: (subscriptionsList: Subscription[]) => void =
+    completeSubscriptions;
+
+  constructor(
+    protected _configurationService: IndexConfigurationDataService,
+    protected _dataService: ProjectDataService,
+    protected _loadingService: RtLoadingService,
+    private _rtDialogService: RtDialogService,
+    private _router: Router,
+    private _customerDataService: CustomerDataService,
+  ) {
+    super();
+  }
+
+  override ngOnInit(): void {
+    this._setSubscriptions();
+  }
+
+  override ngOnDestroy(): void {
+    this._completeSubscriptions(this.subscriptionsList);
+  }
+
+  override _setSubscriptions(): void {
+    this.subscriptionsList.push(this._getProjects());
+  }
+
+  _getProjects(): Subscription {
+    return this._dataService
+      .getMany({})
+      .subscribe((projects: ApiPaginatedResponse<ProjectReadDto>) => {
+        this.projects = projects.data;
+        this.projects.forEach((project) => {
+          this.subscriptionsList.push(this._getProjectWithCustomer(project));
+        });
+      });
+  }
+
+  _getProjectWithCustomer(project: ProjectReadDto): Subscription {
+    return this._customerDataService
+      .getOne(project.customerId)
+      .subscribe((customer: any) => {
+        this.projectsWithCustomer.push({
+          project: project,
+          customer: customer,
+        });
+      });
+  }
+
+  openDialogFn($event: ProjectReadDto): void {
+    this.openDialog.emit($event);
+    this._router.navigate([this._router.url + '/' + $event._id]);
+  }
+}
