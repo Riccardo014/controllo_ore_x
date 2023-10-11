ba22d70afc8abb9801caef71e93754f2e161061c
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-10-11 15:50:35 +0200

    Feat: added release table header component

---
+++ apps/controllo-ore-x/src/app/modules/auth/modules/project/modules/release/components/release-table-header/release-table-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'controllo-ore-x-release-table-header',
+  templateUrl: './release-table-header.component.html',
+  styleUrls: ['./release-table-header.component.scss'],
+})
+export class ReleaseTableHeaderComponent {}
