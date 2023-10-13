fa4991b7428784ffa27c29479b04358ff83ec632
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-10-13 16:23:53 +0200

    Feat: added report index page

--- apps/controllo-ore-x/src/app/modules/auth/modules/report/report.module.ts
+++ apps/controllo-ore-x/src/app/modules/auth/modules/report/report.module.ts
@@ -1,9 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
-import { SharedModule } from '@app/_shared/shared.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { SharedModule } from '@app/_shared/shared.module';
 import {
   RtDialogModule,
   RtInputModule,
@@ -11,19 +22,8 @@ import {
   RtSelectModule,
   RtTableModule,
 } from '@controllo-ore-x/rt-shared';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { ReportRoutingModule } from './report-routing.module';
 import { ReportIndexPage } from './pages/report-index/report-index.page';
+import { ReportRoutingModule } from './report-routing.module';
 
 @NgModule({
   declarations: [ReportIndexPage],
