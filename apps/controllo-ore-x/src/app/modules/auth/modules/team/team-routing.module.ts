c31ab8577e9fe7453021f5521dad7cb67f27b5b9
Riccardo Rossi <riky42001@gmail.com>
2023-10-16 14:59:49 +0200

    Chore: deleted old upsert page for team

--- apps/controllo-ore-x/src/app/modules/auth/modules/team/team-routing.module.ts
+++ apps/controllo-ore-x/src/app/modules/auth/modules/team/team-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TeamUpsertPage } from './dialogs/team-upsert/team-upsert.page';
 import { TeamIndexPage } from './pages/team-index.page';
 
 const routes: Routes = [
   {
     path: '',
     component: TeamIndexPage,
-  },
-  {
-    path: 'create',
-    component: TeamUpsertPage,
-  },
-  {
-    path: ':id',
-    component: TeamUpsertPage,
   },
 ];
 
