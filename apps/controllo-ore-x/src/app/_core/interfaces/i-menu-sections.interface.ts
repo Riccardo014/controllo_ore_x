aaac1962fb2f7b0b97628a3cb3863cae44bfdde0
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-10-20 09:56:45 +0200

    Docs: added docstrings to IMenuSections interface

--- apps/controllo-ore-x/src/app/_core/interfaces/i-menu-sections.interface.ts
+++ apps/controllo-ore-x/src/app/_core/interfaces/i-menu-sections.interface.ts
@@ -1,5 +1,17 @@
+/**
+ * Interface for menu sections
+ */
 export interface IMenuSections {
+  /**
+   * Label for menu section
+   */
   label: string;
-  routerLink?: string;
+  /**
+   * The router link for menu section
+   */
+  routerLink: string;
+  /**
+   * The icon for menu section
+   */
   iconName: string;
 }
