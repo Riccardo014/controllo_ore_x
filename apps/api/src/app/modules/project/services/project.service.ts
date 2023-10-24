c2fba53ff4b1dc8790952e796bbe2d0da3dd8fe0
Riccardo Rossi <riky42001@gmail.com>
2023-10-24 08:53:50 +0200

    Fix: fixed project service

--- apps/api/src/app/modules/project/services/project.service.ts
+++ apps/api/src/app/modules/project/services/project.service.ts
@@ -1,42 +1,48 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CrudService } from '@shared/classes/crud-service.class';
-import { Project } from '@modules/project/entities/project.entity';
 import { ProjectCreateDtoV } from '@modules/project/dtov/project-create.dtov';
 import { ProjectUpdateDtoV } from '@modules/project/dtov/project-update.dtov';
+import { Project } from '@modules/project/entities/project.entity';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CrudService } from '@shared/classes/crud-service.class';
+import { ApiErrors } from '@shared/utils/errors/api-errors';
 import { EntityManager } from 'typeorm';
 import { ReleaseService } from './release.service';
-import { ApiErrors } from '@shared/utils/errors/api-errors';
 
 @Injectable()
-export class ProjectService extends CrudService<Project, ProjectCreateDtoV, ProjectUpdateDtoV> {
+export class ProjectService extends CrudService<
+  Project,
+  ProjectCreateDtoV,
+  ProjectUpdateDtoV
+> {
   target: typeof Project = Project;
 
-  constructor(
-    private _releaseService: ReleaseService
-  ) {
+  constructor(private _releaseService: ReleaseService) {
     super();
   }
 
   async create(data: ProjectCreateDtoV, TX?: EntityManager): Promise<Project> {
     return this.transactionWrap(async (TX: EntityManager) => {
       const project: Project = await super.create(data, TX);
-      
-      if(!project){
+
+      if (!project) {
         throw new BadRequestException(ApiErrors.PROJECT_CREATION_WENT_WRONG);
       }
 
-      const releaseData = {
-        projectId: project._id,
-        version: '0.0.1',
-        isCompleted: false,
-        hoursBudget: project.hoursBudget,
-        billableHoursBudget: project.hoursBudget,
-        deadline: project.deadline, };
-        
-        await this._releaseService.create( releaseData, TX );
+      const releaseCreateData = this.formatReleaseCreateData(project);
+
+      await this._releaseService.create(releaseCreateData, TX);
 
       return project;
     }, TX);
   }
 
+  formatReleaseCreateData(project: Project): any {
+    return {
+      projectId: project._id,
+      version: '0.0.1',
+      isCompleted: false,
+      hoursBudget: project.hoursBudget,
+      billableHoursBudget: project.hoursBudget,
+      deadline: project.deadline,
+    };
+  }
 }
