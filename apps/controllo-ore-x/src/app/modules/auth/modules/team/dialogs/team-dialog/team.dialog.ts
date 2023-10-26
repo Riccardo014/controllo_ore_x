2be9b1510bd82a19eb80e96a7834ccf22e1b73e8
Riccardo Rossi <riky42001@gmail.com>
2023-10-26 17:07:23 +0200

    Feat: updated TeamDialog

--- apps/controllo-ore-x/src/app/modules/auth/modules/team/dialogs/team-dialog/team.dialog.ts
+++ apps/controllo-ore-x/src/app/modules/auth/modules/team/dialogs/team-dialog/team.dialog.ts
@@ -1,23 +1,31 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 import {
   ApiPaginatedResponse,
   RoleReadDto,
-  UserCreateDto,
   UserReadDto,
-  UserUpdateDto,
 } from '@api-interfaces';
 import { RoleDataService } from '@app/_core/services/role.data-service';
 import { TeamDataService } from '@app/_core/services/team.data-service';
-import { BaseDialog } from '@app/_shared/classes/base-dialog.class';
+import { strongPasswordValidator } from '@app/_shared/validators/strong-password.validator';
 import {
   SubscriptionsLifecycle,
   completeSubscriptions,
 } from '@app/utils/subscriptions_lifecycle';
-import { IRtDialogInput, RtDialogService } from '@controllo-ore-x/rt-shared';
+import {
+  IRtDialogClose,
+  IRtDialogInput,
+  RT_DIALOG_CLOSE_RESULT,
+  RtDialogService,
+} from '@controllo-ore-x/rt-shared';
 import { AlertService } from 'libs/rt-shared/src/alert/services/alert.service';
+import { RT_FORM_ERRORS, RtFormError } from 'libs/utils';
 import { Subscription } from 'rxjs';
 import { TeamFormHelper } from '../../helpers/team.form-helper';
 
@@ -27,58 +35,196 @@ import { TeamFormHelper } from '../../helpers/team.form-helper';
   styleUrls: ['./team.dialog.scss'],
   providers: [TeamFormHelper],
 })
-export class TeamDialog
-  extends BaseDialog<UserReadDto, UserCreateDto, UserUpdateDto>
-  implements SubscriptionsLifecycle, OnDestroy, OnInit
-{
-  override title: string = 'Crea nuovo membro';
+export class TeamDialog implements SubscriptionsLifecycle, OnDestroy, OnInit {
+  title: string = 'Crea nuovo membro';
   isPasswordVisible: boolean = false;
+  transactionStatus: 'create' | 'update' = 'create';
+  RT_FORM_ERRORS: { [key: string]: RtFormError } = RT_FORM_ERRORS;
 
-  currentRole?: RoleReadDto;
+  isLoading: boolean = false;
+  hasErrors: boolean = false;
+
+  user?: UserReadDto;
+
+  userFormGroup: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    surname: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    role: new FormControl(null, Validators.required),
+  });
+
+  currentUserRole?: RoleReadDto;
   userRoles: RoleReadDto[] = [];
 
   subscriptionsList: Subscription[] = [];
 
-  _completeSubscriptions: (subscriptionsList: Subscription[]) => void =
+  completeSubscriptions: (subscriptionsList: Subscription[]) => void =
     completeSubscriptions;
 
   constructor(
-    public override formHelper: TeamFormHelper,
-    protected override _formBuilder: FormBuilder,
-    protected _matDialogRef: MatDialogRef<TeamDialog>,
     private _teamDataService: TeamDataService,
-    private _rtDialogService: RtDialogService,
+    public dialogRef: MatDialogRef<TeamDialog>,
     private _alertService: AlertService,
-    private _router: Router,
+    @Inject(MAT_DIALOG_DATA)
+    public data: IRtDialogInput<any>,
+    private _rtDialogService: RtDialogService,
+
+    protected _formBuilder: FormBuilder,
     private _roleDataService: RoleDataService,
-    @Inject(MAT_DIALOG_DATA) public data: IRtDialogInput<any>,
   ) {
-    super(formHelper, _formBuilder, _rtDialogService, _alertService, _router);
-  }
-
-  ngOnInit(): void {
-    this._setSubscriptions();
-
     if (!this.data.input) {
-      this.formHelper.setCreationMode();
+      this._setCreationMode();
       return;
     }
     this.transactionStatus = 'update';
-    this.formHelper.patchForm(this.data.input);
-    this.formHelper.entityId = this.data.input._id;
     this.title = 'Modifica membro';
+    this.user = this.data.input;
+    this.userFormGroup.patchValue(this.data.input);
+  }
+
+  ngOnInit(): void {
+    this.setSubscriptions();
   }
 
   ngOnDestroy(): void {
-    this._completeSubscriptions(this.subscriptionsList);
+    this.completeSubscriptions(this.subscriptionsList);
   }
 
-  _setSubscriptions(): void {
+  setSubscriptions(): void {
     this.subscriptionsList.push(this._getUsersRoles());
+  }
+
+  onDelete(): void {
+    this._rtDialogService
+      .openConfirmation(
+        "Procedere con l'eliminazione?",
+        "L'operazione non è reversibile",
+      )
+      .subscribe({
+        next: (res) => {
+          if (res?.result === RT_DIALOG_CLOSE_RESULT.CONFIRM) {
+            this._delete();
+          }
+        },
+      });
+  }
+
+  onCancel(): void {
+    const modalRes: IRtDialogClose = {
+      result: RT_DIALOG_CLOSE_RESULT.CANCEL,
+    };
+    this.dialogRef.close(modalRes);
+  }
+
+  onSubmit(): void {
+    this.hasErrors = false;
+
+    if (this.transactionStatus === 'update') {
+      this._update();
+      return;
+    }
+
+    this._create();
+  }
+
+  getFormControlError(field: string, error: Error): boolean {
+    return this.userFormGroup.controls[field].hasError(error.name);
+  }
+
+  compareRoleFn(x: RoleReadDto, y: RoleReadDto): boolean {
+    return x?._id === y?._id;
   }
 
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
+  }
+  /**
+   * Set the form in creation mode, adding the password field.
+   */
+  private _setCreationMode(): void {
+    this.userFormGroup.addControl(
+      'password',
+      this._formBuilder.control(null, [
+        Validators.required,
+        strongPasswordValidator,
+      ]),
+    );
+  }
+
+  private _create(): void {
+    const userDto: UserReadDto & { password: string } =
+      this.userFormGroup.getRawValue();
+
+    this.isLoading = true;
+    this.hasErrors = false;
+    this._teamDataService.create(userDto).subscribe({
+      next: () => {
+        this._alertService.openSuccess();
+        const modalRes: IRtDialogClose = {
+          result: RT_DIALOG_CLOSE_RESULT.CONFIRM,
+        };
+        this.dialogRef.close(modalRes);
+      },
+      error: () => {
+        this.hasErrors = true;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  private _update(): void {
+    if (!this.user || this.transactionStatus === 'create') {
+      return;
+    }
+
+    const userId: string = this.user._id;
+    const userDto: UserReadDto = this.userFormGroup.getRawValue();
+
+    this.isLoading = true;
+    this.hasErrors = false;
+    this._teamDataService.update(userId, userDto).subscribe({
+      next: () => {
+        this._alertService.openSuccess();
+        const modalRes: IRtDialogClose = {
+          result: RT_DIALOG_CLOSE_RESULT.CONFIRM,
+        };
+        this.dialogRef.close(modalRes);
+      },
+      error: () => {
+        this.hasErrors = true;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  private _delete(): void {
+    if (!this.user) {
+      return;
+    }
+
+    const userId: string = this.user._id;
+
+    this.isLoading = true;
+    this.hasErrors = false;
+    this._teamDataService.delete(userId).subscribe({
+      next: () => {
+        this._alertService.openSuccess();
+        const modalRes: IRtDialogClose = {
+          result: RT_DIALOG_CLOSE_RESULT.CONFIRM,
+        };
+        this.dialogRef.close(modalRes);
+      },
+      error: () => {
+        this.hasErrors = true;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
   }
 
   /**
@@ -88,12 +234,6 @@ export class TeamDialog
     return this._roleDataService.getMany({}).subscribe({
       next: (roles: ApiPaginatedResponse<RoleReadDto>) => {
         this.userRoles = roles.data;
-        if (this.formHelper.form.value.role) {
-          this.currentRole = this.userRoles.find(
-            (role: RoleReadDto) =>
-              role._id === this.formHelper.form.value.role._id,
-          );
-        }
       },
       error: (error: any) => {
         throw new Error(error);
