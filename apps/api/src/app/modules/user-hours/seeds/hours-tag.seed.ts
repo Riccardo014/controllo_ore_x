89590957e39d9a874a5c011cefee20812a1a7ab5
Riccardo Rossi <riky42001@gmail.com>
2023-11-02 13:01:47 +0100

    Feat: added default hourstag to dayoffs

--- apps/api/src/app/modules/user-hours/seeds/hours-tag.seed.ts
+++ apps/api/src/app/modules/user-hours/seeds/hours-tag.seed.ts
@@ -1,5 +1,6 @@
 export const HOURS_TAG_SEED: any = [
   {
+    _id: '1dac2eaf-c963-4dcd-b7ab-53b366082940',
     name: 'Ferie e permessi',
     iconName: 'carpenter',
     isModifiable: false,
--- apps/api/src/app/modules/user-hours/services/dayoff.service.ts
+++ apps/api/src/app/modules/user-hours/services/dayoff.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { CrudService } from '@shared/classes/crud-service.class';
-import { Dayoff } from '@modules/user-hours/entities/dayoff.entity';
 import { DayoffCreateDtoV } from '@modules/user-hours/dtov/dayoff-create.dtov';
 import { DayoffUpdateDtoV } from '@modules/user-hours/dtov/dayoff-update.dtov';
+import { Dayoff } from '@modules/user-hours/entities/dayoff.entity';
+import { Injectable } from '@nestjs/common';
+import { CrudService } from '@shared/classes/crud-service.class';
+import { EntityManager } from 'typeorm';
+import { HOURS_TAG_SEED } from '../seeds/hours-tag.seed';
 
 @Injectable()
-export class DayoffService extends CrudService<Dayoff, DayoffCreateDtoV, DayoffUpdateDtoV> {
+export class DayoffService extends CrudService<
+  Dayoff,
+  DayoffCreateDtoV,
+  DayoffUpdateDtoV
+> {
   target: typeof Dayoff = Dayoff;
+
+  create(data: DayoffCreateDtoV, TX?: EntityManager): Promise<Dayoff> {
+    const newData = {
+      ...data,
+      hoursTagId: HOURS_TAG_SEED[0]._id,
+    };
+    return this.getRepository(TX).save(newData);
+  }
 }
