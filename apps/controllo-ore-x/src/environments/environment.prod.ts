a320a83312a563d0b522ac5c0b66f16a73989c62
RTsosssoldi <luca.bertolini@raintonic.com>
2023-11-02 16:08:38 +0100

    fix: adjust deploy var

--- apps/controllo-ore-x/src/environments/environment.prod.ts
+++ apps/controllo-ore-x/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { IAngularEnvironment } from '@app/_shared/interfaces/i-angular-environme
 
 export const environment: IAngularEnvironment = {
   production: true,
-  apiUri: 'http://10.100.3.2:3000/api',
+  apiUri: 'https://tpca-2d830a68c047.herokuapp.com/api',
 };
